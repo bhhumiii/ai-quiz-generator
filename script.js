2e1256984d84ace6bbcf63df2bb8e255572d58ff
bhhumiii <bhhumitiwari108@gmail.com>
2025-12-25 18:44:32 +0530

    Correct frontend files at root

---
+++ script.js
@@ -0,0 +1,18 @@
+function sendImage() {
+    const file = document.getElementById("image").files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+
+    fetch("https://ai-quiz-generator-586x.onrender.com/generate-quiz", {
+        method: "POST",
+        body: formData
+    })
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("quiz").innerText = data.quiz;
+        })
+        .catch(err => {
+            alert("Error generating quiz");
+            console.error(err);
+        });
+}
